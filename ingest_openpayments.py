f433f52b47c149a3fc74d1750a6066eb85d1d896
yubinmimi <yubinmimi>
2024-06-24 15:10:54 +0000

    added mimi variables

--- ingest_openpayments.py
+++ ingest_openpayments.py
@@ -9,25 +9,22 @@
 
 # COMMAND ----------
 
-from pathlib import Path
-import re
-import csv
-from pyspark.sql.types import IntegerType
-from pyspark.sql.functions import col, lit, to_date, regexp_replace
-from datetime import datetime
-from dateutil.parser import parse
-import pandas as pd
-
-path = "/Volumes/mimi_ws_1/openpayments/src/main" # where all the input files are located
-catalog = "mimi_ws_1" # delta table destination catalog
-schema = "openpayments" # delta table destination schema
-
+# MAGIC %run /Workspace/Repos/yubin.park@mimilabs.ai/mimi-common-utils/ingestion_utils
 
 # COMMAND ----------
 
-def change_header(header_org):
-    return [re.sub(r'\W+', '', column.lower().replace(' ','_'))
-            for column in header_org]
+# MAGIC %sql
+# MAGIC --DROP TABLE IF EXISTS mimi_ws_1.openpayments.general;
+# MAGIC --DROP TABLE IF EXISTS mimi_ws_1.openpayments.research;
+# MAGIC --DROP TABLE IF EXISTS mimi_ws_1.openpayments.ownership;
+# MAGIC --DROP TABLE IF EXISTS mimi_ws_1.openpayments.deleted;
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, lit, to_date, regexp_replace, when
+catalog = "mimi_ws_1" # delta table destination catalog
+schema = "openpayments" # delta table destination schema
+path = f"/Volumes/mimi_ws_1/{schema}/src/main" # where all the input files are located
 
 # COMMAND ----------
 
@@ -40,33 +37,17 @@ tablename = "general" # destination table
 
 # COMMAND ----------
 
-# We want to skip those files that are already in the delta tables.
-# We look up the table, and see if the files are already there or not.
-files_exist = {}
-writemode = "overwrite"
-if spark.catalog.tableExists(f"{catalog}.{schema}.{tablename}"):
-    files_exist = set([row["_input_file_date"] 
-                   for row in 
-                   (spark.read.table(f"{catalog}.{schema}.{tablename}")
-                            .select("_input_file_date")
-                            .distinct()
-                            .collect())])
-    writemode = "append"
-
-# COMMAND ----------
-
 files = []
 for filepath in Path(f"{path}").glob("OP_DTL_GNRL_PGYR*"):
     year = filepath.stem[16:20]
     dt = parse(f"{year}-12-31").date()
-    if dt not in files_exist:
-        files.append((dt, filepath))
+    files.append((dt, filepath))
 files = sorted(files, key=lambda x: x[0], reverse=True)
 
 # COMMAND ----------
 
 int_columns = {"number_of_payments_included_in_total_amount", "program_year"}
-double_columns = {"total_amount_of_payment_usdollars"}
+double_columns = {"total_amount_of_payment_us_dollars"}
 date_columns = {"payment_publication_date", "date_of_payment"}
 legacy_columns = {}
 
@@ -75,6 +56,8 @@ for item in files:
     # so we load the data using spark one by one just in case
     # the size hits a single machine memory
     df = (spark.read.format("csv")
+            .option("quote", "\"")
+            .option("escape", "\"")
             .option("header", "true")
             .load(str(item[1])))
     header = []
@@ -82,28 +65,30 @@ for item in files:
         
         col_new = legacy_columns.get(col_new_, col_new_)
         header.append(col_new)
-        
         if col_new in int_columns:
-            df = df.withColumn(col_new, regexp_replace(col(col_old), "[\$,%]", "").cast("int"))
+            df = df.withColumn(col_new, col(col_old).cast("int"))
         elif col_new in double_columns:
-            df = df.withColumn(col_new, regexp_replace(col(col_old), "[\$,%]", "").cast("double"))
+            df = df.withColumn(col_new, col(col_old).cast("double"))
         elif col_new in date_columns:
             df = df.withColumn(col_new, to_date(col(col_old), "MM/dd/yyyy"))
         else:
             df = df.withColumn(col_new, col(col_old))
             
     df = (df.select(*header)
-          .withColumn("_input_file_date", lit(item[0])))
+          .withColumn("mimi_src_file_date", lit(item[0]))
+          .withColumn("mimi_src_file_name", lit(item[1].name))
+          .withColumn("mimi_dlt_load_date", lit(datetime.today().date())))
     
     # Some of these are manually corrected above. However, just in case
     # we make the mergeSchema option "true"
+    mimi_src_date = item[0].strftime('%Y-%m-%d')
     (df.write
         .format('delta')
-        .mode(writemode)
+        .mode("overwrite")
         .option("mergeSchema", "true")
+        .option("replaceWhere", f"mimi_src_file_date = '{mimi_src_date}'")
         .saveAsTable(f"{catalog}.{schema}.{tablename}"))
-    
-    writemode="append"
+
 
 # COMMAND ----------
 
@@ -116,24 +101,11 @@ tablename = "research" # destination table
 
 # COMMAND ----------
 
-# We want to skip those files that are already in the delta tables.
-# We look up the table, and see if the files are already there or not.
-files_exist = {}
-writemode = "overwrite"
-if spark.catalog.tableExists(f"{catalog}.{schema}.{tablename}"):
-    files_exist = set([row["_input_file_date"] 
-                   for row in 
-                   (spark.read.table(f"{catalog}.{schema}.{tablename}")
-                            .select("_input_file_date")
-                            .distinct()
-                            .collect())])
-    writemode = "append"
 files = []
 for filepath in Path(f"{path}").glob("OP_DTL_RSRCH_PGYR*"):
     year = filepath.stem[17:21]
     dt = parse(f"{year}-12-31").date()
-    if dt not in files_exist:
-        files.append((dt, filepath))
+    files.append((dt, filepath))
 files = sorted(files, key=lambda x: x[0], reverse=True)
 
 # COMMAND ----------
@@ -148,6 +120,8 @@ for item in files:
     # so we load the data using spark one by one just in case
     # the size hits a single machine memory
     df = (spark.read.format("csv")
+            .option("quote", "\"")
+            .option("escape", "\"")
             .option("header", "true")
             .load(str(item[1])))
     header = []
@@ -157,26 +131,29 @@ for item in files:
         header.append(col_new)
         
         if col_new in int_columns:
-            df = df.withColumn(col_new, regexp_replace(col(col_old), "[\$,%]", "").cast("int"))
+            df = df.withColumn(col_new, col(col_old).cast("int"))
         elif col_new in double_columns:
-            df = df.withColumn(col_new, regexp_replace(col(col_old), "[\$,%]", "").cast("double"))
+            df = df.withColumn(col_new, col(col_old).cast("double"))
         elif col_new in date_columns:
             df = df.withColumn(col_new, to_date(col(col_old), "MM/dd/yyyy"))
         else:
             df = df.withColumn(col_new, col(col_old))
             
     df = (df.select(*header)
-          .withColumn("_input_file_date", lit(item[0])))
+          .withColumn("mimi_src_file_date", lit(item[0]))
+          .withColumn("mimi_src_file_name", lit(item[1].name))
+          .withColumn("mimi_dlt_load_date", lit(datetime.today().date())))
     
     # Some of these are manually corrected above. However, just in case
     # we make the mergeSchema option "true"
+    mimi_src_date = item[0].strftime('%Y-%m-%d')
     (df.write
         .format('delta')
-        .mode(writemode)
+        .mode("overwrite")
         .option("mergeSchema", "true")
+        .option("replaceWhere", f"mimi_src_file_date = '{mimi_src_date}'")
         .saveAsTable(f"{catalog}.{schema}.{tablename}"))
-    
-    writemode="append"
+
 
 # COMMAND ----------
 
@@ -189,24 +166,11 @@ tablename = "ownership" # destination table
 
 # COMMAND ----------
 
-# We want to skip those files that are already in the delta tables.
-# We look up the table, and see if the files are already there or not.
-files_exist = {}
-writemode = "overwrite"
-if spark.catalog.tableExists(f"{catalog}.{schema}.{tablename}"):
-    files_exist = set([row["_input_file_date"] 
-                   for row in 
-                   (spark.read.table(f"{catalog}.{schema}.{tablename}")
-                            .select("_input_file_date")
-                            .distinct()
-                            .collect())])
-    writemode = "append"
 files = []
 for filepath in Path(f"{path}").glob("OP_DTL_OWNRSHP_PGYR*"):
     year = filepath.stem[19:23]
     dt = parse(f"{year}-12-31").date()
-    if dt not in files_exist:
-        files.append((dt, filepath))
+    files.append((dt, filepath))
 files = sorted(files, key=lambda x: x[0], reverse=True)
 
 # COMMAND ----------
@@ -221,6 +185,8 @@ for item in files:
     # so we load the data using spark one by one just in case
     # the size hits a single machine memory
     df = (spark.read.format("csv")
+            .option("quote", "\"")
+            .option("escape", "\"")
             .option("header", "true")
             .load(str(item[1])))
     header = []
@@ -230,26 +196,28 @@ for item in files:
         header.append(col_new)
         
         if col_new in int_columns:
-            df = df.withColumn(col_new, regexp_replace(col(col_old), "[\$,%]", "").cast("int"))
+            df = df.withColumn(col_new, col(col_old).cast("int"))
         elif col_new in double_columns:
-            df = df.withColumn(col_new, regexp_replace(col(col_old), "[\$,%]", "").cast("double"))
+            df = df.withColumn(col_new, col(col_old).cast("double"))
         elif col_new in date_columns:
             df = df.withColumn(col_new, to_date(col(col_old), "MM/dd/yyyy"))
         else:
             df = df.withColumn(col_new, col(col_old))
             
     df = (df.select(*header)
-          .withColumn("_input_file_date", lit(item[0])))
+          .withColumn("mimi_src_file_date", lit(item[0]))
+          .withColumn("mimi_src_file_name", lit(item[1].name))
+          .withColumn("mimi_dlt_load_date", lit(datetime.today().date())))
     
     # Some of these are manually corrected above. However, just in case
     # we make the mergeSchema option "true"
+    mimi_src_date = item[0].strftime('%Y-%m-%d')
     (df.write
         .format('delta')
-        .mode(writemode)
+        .mode("overwrite")
         .option("mergeSchema", "true")
+        .option("replaceWhere", f"mimi_src_file_date = '{mimi_src_date}'")
         .saveAsTable(f"{catalog}.{schema}.{tablename}"))
-    
-    writemode="append"
 
 # COMMAND ----------
 
@@ -262,25 +230,11 @@ tablename = "deleted"
 
 # COMMAND ----------
 
-# We want to skip those files that are already in the delta tables.
-# We look up the table, and see if the files are already there or not.
-files_exist = {}
-writemode = "overwrite"
-if spark.catalog.tableExists(f"{catalog}.{schema}.{tablename}"):
-    files_exist = set([row["_input_file_date"] 
-                   for row in 
-                   (spark.read.table(f"{catalog}.{schema}.{tablename}")
-                            .select("_input_file_date")
-                            .distinct()
-                            .collect())])
-    writemode = "append"
 files = []
 for filepath in Path(f"{path}").glob("OP_REMOVED_DELETED_PGYR*"):
-    
     year = filepath.stem[23:27]
     dt = parse(f"{year}-12-31").date()
-    if dt not in files_exist:
-        files.append((dt, filepath))
+    files.append((dt, filepath))
 files = sorted(files, key=lambda x: x[0], reverse=True)
 
 # COMMAND ----------
@@ -293,6 +247,8 @@ for item in files:
     # so we load the data using spark one by one just in case
     # the size hits a single machine memory
     df = (spark.read.format("csv")
+            .option("quote", "\"")
+            .option("escape", "\"")
             .option("header", "true")
             .load(str(item[1])))
     header = []
@@ -302,22 +258,24 @@ for item in files:
         header.append(col_new)
         
         if col_new in int_columns:
-            df = df.withColumn(col_new, regexp_replace(col(col_old), "[\$,%]", "").cast("int"))
+            df = df.withColumn(col_new, col(col_old).cast("int"))
         else:
             df = df.withColumn(col_new, col(col_old))
             
     df = (df.select(*header)
-          .withColumn("_input_file_date", lit(item[0])))
+          .withColumn("mimi_src_file_date", lit(item[0]))
+          .withColumn("mimi_src_file_name", lit(item[1].name))
+          .withColumn("mimi_dlt_load_date", lit(datetime.today().date())))
     
     # Some of these are manually corrected above. However, just in case
     # we make the mergeSchema option "true"
+    mimi_src_date = item[0].strftime('%Y-%m-%d')
     (df.write
         .format('delta')
-        .mode(writemode)
+        .mode("overwrite")
         .option("mergeSchema", "true")
+        .option("replaceWhere", f"mimi_src_file_date = '{mimi_src_date}'")
         .saveAsTable(f"{catalog}.{schema}.{tablename}"))
-    
-    writemode="append"
 
 # COMMAND ----------
 
